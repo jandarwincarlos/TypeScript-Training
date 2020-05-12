fe9065ea6e61a050b7f031e9222d4f0e6a71b8ee
jandarwincarlos <jdcarlos@renditiondigital.com.ph>
2020-05-13 06:18:42 +0800

    Installation and Setup  - Using Visual Studio Code

---
+++ TypeScript In-depth/app.ts
@@ -0,0 +1,8 @@
+class HelloWorld {
+    constructor(public message: string) { }
+}
+
+var hello = new HelloWorld('Hello TypeScript 2!');
+console.log(hello.message);
+
+console.log('Hello Visual Studio Code 2');
---
+++ TypeScript In-depth/js/app.js
@@ -0,0 +1,11 @@
+"use strict";
+var HelloWorld = /** @class */ (function () {
+    function HelloWorld(message) {
+        this.message = message;
+    }
+    return HelloWorld;
+}());
+var hello = new HelloWorld('Hello TypeScript 2!');
+console.log(hello.message);
+console.log('Hello Visual Studio Code 2');
+//# sourceMappingURL=app.js.map
